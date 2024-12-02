3ad95d7f87bb342efefcfdbba033d37e3c02a501
Alejandrito8 <aleksandar.jurkovic@edu.newton.se>
2024-12-02 20:39:43 +0100

    A commit to save changes

--- Inläming_DataBas/Models/AddData.cs
+++ Inläming_DataBas/Models/AddData.cs
@@ -7,29 +7,79 @@ public class AddData
     {
         using (var context = new AppDbContext())
         {
+            try
+            {
+            System.Console.WriteLine("Enter the first name for the author");
+            string firstName = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(firstName))
+                {
+                    throw new ArgumentException("First name cannot be empty");
+                }
+            Console.WriteLine("Enter the last name of the author");
+            string lastName = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(lastName))
+                {
+                    throw new ArgumentException("Last name cannot be empty");
+                }
+            Console.WriteLine("Enter the date of birth of the author (yyyy-mm-dd)");
+            if (!DateTime.TryParse(Console.ReadLine(), out DateTime dateOfBirth))
+            {
+                Console.WriteLine("Invalid date format. Please use yyyy-mm-dd");
+                return;
+            }
             var author = new Author
             {
-                FirstName = "Aleksandar",
-                LastName = "Jurkovic",
-                DateOfBirth = new DateTime(1775, 12, 16)
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = dateOfBirth
             };
             context.Authors.Add(author);
             context.SaveChanges();
+            Console.WriteLine($"Author {firstName} {lastName} born {dateOfBirth} has been added");
+            }
+            catch (Exception ex)
+            {                
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
     public static void CreateBook()
     {
         using (var context = new AppDbContext())
         {
-            var book = new Book
-
+             try
             {
-                Title = "Aleksandar The Great",
-                PublicationYear = 1813,
-                Genre = "ActionDrama",
+            System.Console.WriteLine("Enter the title of the book");
+            string title = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(title))
+                {
+                    throw new ArgumentException("Title cannot be empty");
+                }
+            Console.WriteLine("Enter the genre of the book");
+            string genre = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(genre))
+                {
+                    throw new ArgumentException("Genre cannot be empty");
+                }
+            Console.WriteLine("Enter the publication year of the book");
+            if (!int.TryParse(Console.ReadLine(), out int publicationYear) || publicationYear < 0)
+            {
+                throw new ArgumentException ("Invalid date format. Please use yyyy-mm-dd");
+            }
+             var book = new Book
+            {
+                Title = title,
+                PublicationYear = publicationYear,
+                Genre = genre,
             };
             context.Books.Add(book);
             context.SaveChanges();
+            Console.WriteLine($"Book with the title {title} published in {publicationYear} with the genre {genre} has been added.");
+            }
+            catch (Exception ex)
+            {                
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
--- Inläming_DataBas/Program.cs
+++ Inläming_DataBas/Program.cs
@@ -1,38 +1,34 @@
 ﻿using System.Diagnostics;
 using Inlämning_DataBas.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-
-        System.Console.WriteLine("What do you want to do?");
-        System.Console.WriteLine("1. Add Data");
-        System.Console.WriteLine("2. Update data");
-        System.Console.WriteLine("3. Delete data");
-        System.Console.WriteLine("4. Quit");
-
-
-
-        var input = Console.ReadLine();
         bool Menu = true;
         while (Menu)
         {
+            System.Console.WriteLine("What do you want to do?");
+            System.Console.WriteLine("1. Add Data");
+            System.Console.WriteLine("2. Update data");
+            System.Console.WriteLine("3. Delete data");
+            System.Console.WriteLine("4. Quit");
+            var input = Console.ReadLine();
             switch (input)
             {
                 case "1":
+                    System.Console.WriteLine("What data do you want to add?");
+                    System.Console.WriteLine("1. Add an Author");
+                    System.Console.WriteLine("2. Add a book");
+                    System.Console.WriteLine("3. Quit");
                     var input1 = Console.ReadLine();
-                    bool Menu1 = true;
-                    while (Menu1)
                     {
-                        System.Console.WriteLine("What data do you want to add?");
-                        System.Console.WriteLine("1. Create an Author");
-                        System.Console.WriteLine("2. Create a book");
-                        System.Console.WriteLine("3. Quit");
                         if (input1 == "1")
                         {
                             AddData.CreateAuthor();
+                            break;
                         }
                         else if (input1 == "2")
                         {
@@ -41,89 +37,89 @@ class Program
                         else if (input1 == "3")
                         {
                             System.Console.WriteLine("Exiting program");
-                            Menu1 = false;
+                            return;
                         }
                         else
                         {
-                            Console.WriteLine("Oops... A problem occured");
+                            System.Console.WriteLine("Oops... A problem occured");
+                            break;
                         }
                     }
                     break;
 
                 case "2":
-                    var input2 = Console.ReadLine();
-                    bool Menu2 = true;
-                    while (Menu2)
-                    {
-                        System.Console.WriteLine("What data do you want to update?");
-                        System.Console.WriteLine("1. Update Book & Author relation");
-                        System.Console.WriteLine("2. Update Book & Borrower ");
-                        System.Console.WriteLine("3. Quit");
 
-                        if (input2 == "1")
-                        {
-                            UppdateData.BookAuthorRelation();
-                        }
-                        else if (input2 == "2")
-                        {
-                            UppdateData.BookLoan();
-                        }
-                        else if (input2 == "3")
-                        {
-                            System.Console.WriteLine("Exiting program");
-                            Menu2 = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Oops... A problem occured");
-                        }
+                    System.Console.WriteLine("What data do you want to update?");
+                    System.Console.WriteLine("1. Update Book & Author relation");
+                    System.Console.WriteLine("2. Update Book & Borrower ");
+                    System.Console.WriteLine("3. Quit");
+                    var input2 = Console.ReadLine();
+
+
+                    if (input2 == "1")
+                    {
+                        UppdateData.BookAuthorRelation();
+                        break;
                     }
-                    break;
+                    else if (input2 == "2")
+                    {
+                        UppdateData.BookLoan();
+                        break;
+                    }
+                    else if (input2 == "3")
+                    {
+                        System.Console.WriteLine("Exiting program");
+                        return;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Oops... A problem occured");
+                        break;
+                    }
 
                 case "3":
+                    System.Console.WriteLine("What data do you want to Delete?");
+                    System.Console.WriteLine("1. Delete Author");
+                    System.Console.WriteLine("2. Delete Book");
+                    System.Console.WriteLine("3. Delete Borrower");
+                    System.Console.WriteLine("4. Quit");
                     var input3 = Console.ReadLine();
-                    bool Menu3 = true;
-                    while (Menu3)
+
+                    if (input3 == "1")
                     {
-                        System.Console.WriteLine("What data do you want to Delete?");
-                        System.Console.WriteLine("1. Delete Author");
-                        System.Console.WriteLine("2. Delete Book");
-                        System.Console.WriteLine("3. Delete Borrower");
-                        System.Console.WriteLine("4. Quit");
-                        if (input3 == "1")
-                        {
-                            DeleteData.DeleteAuthor();
-                        }
-                        else if (input3 == "2")
-                        {
-                            DeleteData.DeleteBook();
-                        }
-                        else if (input3 == "3")
-                        {
-                            DeleteData.DeleteBorrower();
-                        }
-                        else if (input3 == "4")
-                        {
-                            System.Console.WriteLine("Exiting program");
-                            Menu2 = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Oops... A problem occured");
-                        }
+                        DeleteData.DeleteAuthor();
+                        break;
                     }
-                    break;
+                    else if (input3 == "2")
+                    {
+                        DeleteData.DeleteBook();
+                        break;
+                    }
+                    else if (input3 == "3")
+                    {
+                        DeleteData.DeleteBorrower();
+                        break;
+                    }
+                    else if (input3 == "4")
+                    {
+                        System.Console.WriteLine("Exiting program");
+                        return;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Oops... A problem occured");
+                        break;
+                    }
 
                 case "4":
                     System.Console.WriteLine("Exiting program");
-                    Menu = false;
-                    break;
-
-                default:
-                    Console.WriteLine("Oops... A problem occured");
                     return;
 
+                default:
+                    System.Console.WriteLine("Oops... A problem occured");
+                    break;
             }
         }
+
     }
 }
